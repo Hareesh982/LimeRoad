28048867134ad903263439bd890868c963a3ebdc
D Hareesh <hareeshduddupudi9271@gmail.com>
2025-03-15 13:17:06 +0530

    fixing cors error

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,15 @@ dotenv.config()
 
 const app = express()
 
-app.use(
-    cors({
-      origin: "http://15.206.116.161:3000", // Allow this frontend origin
-      methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-      credentials: true, // Allow cookies (if needed)
-    })
-  );
+// app.use(
+//     cors({
+//       origin: "http://15.206.116.161:3000", // Allow this frontend origin
+//       methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+//       credentials: true, // Allow cookies (if needed)
+//     })
+//   );
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
